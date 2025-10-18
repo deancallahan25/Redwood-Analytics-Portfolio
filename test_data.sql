13453904afe8400d8bacfd41a59c64da95e8c692
Jaiden Roe <145399135+m0chii-choco@users.noreply.github.com>
2025-10-18 16:09:08 -0700

    Update test_data.sql

--- test_data.sql
+++ test_data.sql
@@ -1,12 +1,47 @@
---test inserting data into table to see if the errors will show up or not for the building table 
-INSERT INTO Building VALUES(67,100, 'Hi there');
-INSERT INTO Building VALUES(67,100, 'Hi there');
 
--- inserting data into waste audit able to see if errors will show up in the waste audit table
-INSERT INTO Waste_Audit VALUES (11, NULL, 'C9 910', '16',12,TO_DATE('23-32-1320','MM-DD-YYYY'));
+--Testing all the triggers from the design sql statements
 
--- insearting data into the auditor table to show if errors will popped up
-INSERT INTO Auditors VALUES (920, 'jr650@humboldt.edu','Jaiden', 21);
 
---  Categories insert values 
-INSERT INTO Categories VALUES (123, 'LandFill', 10.91, 'asdf', 30);
+-- Building
+
+INSERT INTO Building (BUILDING_ID, building_name, building_type)
+VALUES ('B001', 'Science Hall', 'Academic');
+
+INSERT INTO Building (BUILDING_ID, building_name, building_type)
+VALUES ('B002', 'Main Library', 'Academic');
+
+-- waste audits
+
+INSERT INTO Waste_Audit (building_id, number_bags, total_weight, weight_error, volume_error, date_conducted)
+VALUES ('B001', 10, 25.00, NULL, NULL, TO_DATE('2025-10-10', 'YYYY-MM-DD'));
+
+INSERT INTO Waste_Audit (building_id, number_bags, total_weight, weight_error, volume_error, date_conducted)
+VALUES ('B002', 5, 12.50, NULL, NULL, TO_DATE('2025-10-11', 'YYYY-MM-DD'));
+
+-- auditors
+
+INSERT INTO Auditors (email_address, auditor_fname, auditor_lname)
+VALUES ('dcallahan@redwood.edu', 'Dean', 'Callahan');
+
+INSERT INTO Auditors (email_address, auditor_fname, auditor_lname)
+VALUES ('zgriffiths@redwood.edu', 'Zachary', 'Griffiths');
+
+-- categories
+
+INSERT INTO Categories (category_name, audit_id, category_weight, percent_by_weight, percent_by_volume)
+VALUES ('Recyclables', 100003, 10.00, 40.00, 40.00);
+
+INSERT INTO Categories (category_name, audit_id, category_weight, percent_by_weight, percent_by_volume)
+VALUES ('Compost', 100003, 10.00, 40.00, 40.00);
+
+INSERT INTO Categories (category_name, audit_id, category_weight, percent_by_weight, percent_by_volume)
+VALUES ('Landfill', 100003, 5.00, 20.00, 20.00);
+
+-- audits
+
+INSERT INTO Categories (category_name, audit_id, category_weight, percent_by_weight, percent_by_volume)
+VALUES ('Recyclables', 100004, 4.00, 32.00, 60.00);
+
+INSERT INTO Categories (category_name, audit_id, category_weight, percent_by_weight, percent_by_volume)
+VALUES ('Landfill', 100004, 6.00, 48.00, 30.00);
+
